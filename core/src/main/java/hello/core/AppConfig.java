a68e43377692d7bd18e8925e20bd8d2be7dfa5cc
LJYeon12 <juyeon010530@gmail.com>
2024-10-01 19:39:47 +0900

    1.1 완료

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,21 +9,25 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // 구현체 할당과 같은 환경 설정은 모두 AppConfig 파일 내에서 진행한다
+    @Bean // 스프링 컨테이너에 등록
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
-
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,18 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+/*        AppConfig appConfig = new AppConfig();
         // 궁금한 점 : 이와 같이 할당하면 이것은 OCP,DIP 원칙을 잘 지킨 코드인가? 내가 볼때는 여전히 구현체에 의존하고 있는 것 같은데
-        MemberService memberService = appConfig.memberService();
+        MemberService memberService = appConfig.memberService();*/
+
+        // ApplicationContext : 스프링 컨테이너라 보면된다.
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         // 회원가입
         memberService.join(member);
